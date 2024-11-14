f6eff13aaaa21cc50af6d7afe0d73946b9420a76
elkhailihamza <elkhailihamza331@gmail.com>
2024-11-14 17:21:26 +0100

    added mapper methods to service

--- src/main/java/org/project/Service/LoanService.java
+++ src/main/java/org/project/Service/LoanService.java
@@ -1,6 +1,9 @@
 package org.project.Service;
 
 import org.project.Dao.LoanDao;
+import org.project.Dto.Mapper.LoanMapper;
+import org.project.Dto.request.LoanReqDto;
+import org.project.Dto.response.LoanResDto;
 import org.project.Entity.Loan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,6 +17,22 @@ public class LoanService {
     @Autowired
     public LoanService(LoanDao loanDao) {
         this.loanDao = loanDao;
+    }
+
+    public Loan toLoan(LoanReqDto loanReqDto) {
+        return LoanMapper.INSTANCE.toLoan(loanReqDto);
+    }
+
+    public Loan toLoan(LoanResDto loanResDto) {
+        return LoanMapper.INSTANCE.toLoan(loanResDto);
+    }
+
+    public LoanResDto getLoanToLoanResDto(Loan loan) {
+        return LoanMapper.INSTANCE.getLoanToLoanResDto(loan);
+    }
+
+    public LoanReqDto getLoanToLoanReqDto(Loan loan) {
+        return LoanMapper.INSTANCE.getLoanToLoanReqDto(loan);
     }
 
     public List<Loan> getLoanHistory() {
