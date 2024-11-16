8f481ab80a50595ec2b47833f790dad5e7a17f4a
elkhailihamza <elkhailihamza331@gmail.com>
2024-11-16 12:11:05 +0100

    added exception handle and error details

--- src/main/java/org/project/ebankify/exceptions/GlobalExceptionHandler.java
+++ src/main/java/org/project/ebankify/exceptions/GlobalExceptionHandler.java
@@ -1,4 +1,42 @@
 package org.project.ebankify.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<?> handleEntityNotFoundException(EntityNotFoundException ex, WebRequest webRequest) {
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(InvalidFundsException.class)
+    public ResponseEntity<?> handleInvalidFundsException(InvalidFundsException ex, WebRequest webRequest) {
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    @ExceptionHandler(UnexpectedErrorException.class)
+    public ResponseEntity<?> handleUnexpectedErrorException(UnexpectedErrorException ex, WebRequest webRequest) {
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(EntityDataConflictException.class)
+    public ResponseEntity<?> handleEntityDataConflictException(EntityDataConflictException ex, WebRequest webRequest) {
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(EntityCRUDFailedException.class)
+    public ResponseEntity<?> handleEntityCRUDFailedException(EntityCRUDFailedException ex, WebRequest webRequest) {
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.UNPROCESSABLE_ENTITY);
+    }
 }
